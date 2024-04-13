91992a043d155d85f4c74e822736ac2efaf09c3d
dlleong0017 <dlleong0017@gmail.com>
2024-04-12 23:11:46 -0400

    this is my decoder function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+
 # this is my first github addition
+
+def decode(encoded_password):
+	new_string=""
+	for i in encoded_password:
+		new_string+=string(int(i)-3)
+	
+	return new_string
 
 print("Hello world")
